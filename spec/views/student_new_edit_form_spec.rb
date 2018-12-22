50e3459ccac9fe0de0ea6e65a61f4a08421155a6
Peter Kreppein <pmkreppein@gmail.com>
2018-12-22 00:20:20 +0000

    Automatically backed up by Learn

--- spec/views/student_new_edit_form_spec.rb
+++ spec/views/student_new_edit_form_spec.rb
@@ -1,7 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "create and edit form" do
-  
+  describe 'create form' do
+    it "renders create student form from the new.html file" do
+      view.lookup_context.prefixes = %w[students]
+      student = Student.new
+      assign(:student, student)
+      render :template => "students/new.html.erb"
+      expect(rendered).to match /Create Student/
+    end
 
     it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
       view.lookup_context.prefixes = %w[students]
@@ -29,6 +36,15 @@ RSpec.describe "create and edit form" do
       assign(:student, student)
       render :template => "students/edit.html.erb"
       expect(rendered).to match /Update Student/
+    end
+
+    it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
+      view.lookup_context.prefixes = %w[students]
+      student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+      assign(:student, student)
+      render :template => "students/new.html.erb"
+
+      expect(rendered).to render_template(:partial => "_form")
     end
 
 

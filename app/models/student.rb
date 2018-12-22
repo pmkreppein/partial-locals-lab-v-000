7685567c3251fd38dda0a1b3021bac3695f795fd
Peter Kreppein <pmkreppein@gmail.com>
2018-12-22 00:12:15 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,13 @@
-# == Schema Information
-#
-# Table name: students
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  hometown   :string
-#  birthday   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+ def self.search(query)
+    if query == ""
+      self.all
+    else
+      self.all.select{|s| s.name.downcase.include?(query.downcase)}
+    end
+  end
+
 end
